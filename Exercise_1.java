b88dc2ed5768b824665b3bf6dc85c87244ddaabb
CodeKnight314 <121400022+CodeKnight314@users.noreply.github.com>
2022-12-26 18:01:46 -0800

    Add files via upload

---
+++ Exercise_1.java
@@ -0,0 +1,12 @@
+public class Exercise_one {
+    public static void main(String[] args){
+        String string1 = "Ben";
+        String string2 = "Germany";
+        String string3 = "Mushroom pasta";
+
+        System.out.println(string1.indexOf('e'));
+        System.out.println(string2.indexOf('e'));
+        System.out.println(string3.indexOf('e'));
+
+    }
+}
---
+++ Exercise_6.java
@@ -0,0 +1,14 @@
+public class Exercise_six {
+    public static void main(String[] args){
+        int test1 = 123123;
+        int test2 = -12319;
+
+        //switch test1 with test2 to compare results if needed
+        if(test1 > 0)
+            System.out.println("positive");
+        else if(test1 < 0)
+            System.out.println("negative");
+        else
+            System.out.println("zero");
+    }
+}
