89fc6bb39ba5ce66b13a52dfc631ad1092825bb7
CodeKnight314 <121400022+CodeKnight314@users.noreply.github.com>
2022-12-26 18:01:05 -0800

    Delete Exercise_three.java

--- Exercise_three.java
+++
@@ -1,7 +0,0 @@
-public class Exercise_three {
-    public static void main(String[] args){
-        boolean expression = (10 - 12/4) == (10 * (102+2));
-
-        System.out.println(expression);
-    }
-}
